1bf5fadd720fdd268be78c76583ad388e921a52d
AndreiPerevoshchikov <113555470+AndreiPerevoshchikov@users.noreply.github.com>
2023-06-15 20:21:33 +0300

    Add files via upload

---
+++ search/functions_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['open_5fcsv_5ffile_0',['open_csv_file',['../namespacemain.html#a6660a24a6cfd7a1849e0e95c9fe050b5',1,'main']]]
+];
---
+++ search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['converter_0',['Converter',['../md__c_1_2_users_2perev_2_pycharm_projects_2_converter1_2_r_e_a_d_m_e.html',1,'']]]
+];
